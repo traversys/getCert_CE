0f4213c569b810d372d1419e6998a39c00fa47e4
Wes Moskal-Fitzpatrick <github@fitzmoskal.me>
2021-09-29 19:46:06 +0100

    Updated gitignore

---
+++ lib/cron.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# (c) Copyright 2015-2021, Traversys Limited
+#
+
+source <(grep "root =" config.ini | sed 's/ *= */=/g')
+source <(grep "cron =" config.ini | sed 's/ *= */=/g')
+
+if [ "$TIDEWAY" == "" ]; then
+          export TIDEWAY=/usr/tideway
+fi
+. $TIDEWAY/tw_setup
+
+echo "$cron $root/getcert" >> traversys_getCert.cron
+mv traversys_getCert.cron /usr/tideway/etc/cron/
+tw_cron_update
+echo ""
