f250c7329cef70ee11b7a70e43f902cd268335f3
Wes Moskal-Fitzpatrick <github@fitzmoskal.me>
2021-10-15 17:31:29 +0100

    v1.7.0-dev-15
    - makeself moved to Dockerfile installation
    - standalone crontab update

--- standalone/cron.sh
+++ standalone/cron.sh
@@ -3,14 +3,14 @@
 #
 
 source <(grep "root =" config.ini | sed 's/ *= */=/g')
-source <(grep "cron =" config.ini | sed 's/ *= */=/g')
 
 if [ "$TIDEWAY" == "" ]; then
           export TIDEWAY=/usr/tideway
 fi
 . $TIDEWAY/tw_setup
 
-echo "$cron $root/getcert" >> traversys_getCert.cron
 mv traversys_getCert.cron /usr/tideway/etc/cron/
+echo "# Every evening at 6pm" >> /usr/tideway/etc/cron/traversys_getCert.cron
+echo "0 18 * * * $root/getcert" >> /usr/tideway/etc/cron/traversys_getCert.cron
 tw_cron_update
 echo ""
